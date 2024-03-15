1b83750519b198859ed6d9922e2b0efcfaca645d
unknown <natnaelfekadu801@gmail.com>
2024-03-15 04:44:10 -0700

    a dummy hardcoded data to populate the card

---
+++ learning_path/src/data/dummy_data.ts
@@ -0,0 +1,21 @@
+interface DUMMYDATA {
+  id:number;
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string;
+}
+
+export const DUMMY_DATA: DUMMYDATA[] = [
+  {
+    id:1,
+    title: "Social Media Assistant",
+    subtitle: "Young Men Chrstians Assosiation . Addis Ababa, Ethiopia",
+    description: `As a Social Media Assistant, you will work closely with the social
+    media manager or marketing team to execute social media strategies and
+    companies. You will be responsible for assisting in the creation and
+    scheduling of engaging content, monitoring social media channels, and
+    interacting with flowers.`,
+    image: "./src/assets/logo.svg",
+  },
+];
