a411ed3a05952828834f135c7486299c40be58a4
Natinael Fekadu <101578729+NatinaelFekadu@users.noreply.github.com>
2024-03-21 06:01:20 -0700

    landing page

--- next_router_task/src/app/page.tsx
+++ next_router_task/src/app/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@/components/Button";
 import Link from "next/link";
 
 export default function Home() {
-  return <Link href={"/JobsList"}>JobsList</Link>;
+  return (
+    <Link href={"/JobsList"}>
+      <div className="flex-col justify-around  w-3/5 mx-auto">
+        <div className="text-center mt-5">
+          <p className="font-epilogue ">
+            You can see available opportunities here.
+          </p>
+        </div>
+        <div className="rounded-md bg-white text-center shadow-md p-5">
+          <Button class_name="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Go to JobsList
+          </Button>
+        </div>
+      </div>
+    </Link>
+  );
 }
